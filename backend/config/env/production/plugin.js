5ca2b6b1576ffdb749d0e23eeeec2d8d50f8b7a0
shairy111 <saljoqishairy4@gmail.com>
2021-09-02 16:44:07 +0500

    reverted back to env

--- backend/config/env/production/plugin.js
+++ backend/config/env/production/plugin.js
@@ -1,34 +1,12 @@
 // require("dotenv").config()
-// module.exports = ({ env }) => ({
-//     // ...
-//     upload: {
-//       provider: 'cloudinary',
-//       providerOptions: {
-//         cloud_name: env('CLOUDINARY_NAME'),
-//         api_key: env('CLOUDINARY_KEY'),
-//         api_secret: env('CLOUDINARY_SECRET'),
-//       },
-//       actionOptions: {
-//         upload: {},
-//         delete: {},
-//       },
-//     },
-
-    
-//     // ...
-//   });
-
- 
-
-//imediately change it to env
-  module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
     // ...
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: "duov6f9b3",
-        api_key: "259823422918374",
-        api_secret: "7F-dbOFUJeL9hw4V7-8Xkh42kMY",
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
@@ -39,5 +17,9 @@
     
     // ...
   });
+
+ 
+
+
 
  
