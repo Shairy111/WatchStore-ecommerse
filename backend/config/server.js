9d6329f6ce59bce2b82bb68ad51b53783b5dd3f8
shairy111 <saljoqishairy4@gmail.com>
2021-09-02 16:51:24 +0500

    removed upload folder , commented the development database|server.js files

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '2b0b64074e1852cf929e3df371bf758c'),
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', '2b0b64074e1852cf929e3df371bf758c'),
+//     },
+//   },
+// });
