c30144e6730ce7ea0fc4f22ce013aec7f2f10343
shairy111 <saljoqishairy4@gmail.com>
2021-09-06 09:42:00 +0500

    order content type

--- frontend/src/components/ui/featured.js
+++ frontend/src/components/ui/featured.js
@@ -1,69 +1,53 @@
 //ratings to be extracted from query
-import React from 'react'
-import WatchCard from "./WatchCard"
-import Grid from '@material-ui/core/Grid';
-import { useStaticQuery, graphql } from "gatsby"
-
+import React from "react";
+import WatchCard from "./WatchCard";
+import Grid from "@material-ui/core/Grid";
+import { useStaticQuery, graphql } from "gatsby";
 
 function Featured() {
-
-        const featuredProducts = useStaticQuery(graphql`query MyQuery {
-            allStrapiWatch {
-              nodes {
-                name
-                price
-                on_sale
-                salePercent
-                strapiId
-                images {
-                    url
-                  }
-              }
-            }
+  const featuredProducts = useStaticQuery(graphql`
+    query MyQuery {
+      allStrapiWatch {
+        nodes {
+          name
+          price
+          on_sale
+          salePercent
+          strapiId
+          images {
+            url
           }
-          `)
+        }
+      }
+    }
+  `);
 
-          
-    return (
-
-
-        <>
-        
-        <Grid container>
-        <Grid  item xs={12}>
-            <Grid container justifyContent="flex-start">
-              
-                {featuredProducts.allStrapiWatch.nodes.map((node , idx) => {
-            const fullUrl = node.images[0].url
-            return (
-            
-            
-            <>
-            <Grid key={idx} item>
-            <WatchCard  watchName={node.name} ImageUrl={fullUrl} oldPrice={node.price} salePercent={node.salePercent} path={`/${node.name}/details`}/>
-            </Grid>
-             
-            </>
-           
-            
-            
-            
-            
-            )
-        })}
-                
-      
-            </Grid>
-     
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          <Grid container justifyContent="flex-start">
+            {featuredProducts.allStrapiWatch.nodes.map((node, idx) => {
+              const fullUrl = node.images[0].url;
+              return (
+                <>
+                  <Grid key={idx} item>
+                    <WatchCard
+                      watchName={node.name}
+                      ImageUrl={fullUrl}
+                      oldPrice={node.price}
+                      salePercent={node.salePercent}
+                      path={`/${node.name}/details`}
+                    />
+                  </Grid>
+                </>
+              );
+            })}
+          </Grid>
         </Grid>
-
-        </Grid>
-       
-
-     
-        </>
-        
-    )
+      </Grid>
+    </>
+  );
 }
 
-export default Featured
+export default Featured;
--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import SwipeableDrawer from "@material-ui/core/SwipeableDrawer"
-import List from "@material-ui/core/List"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemText from "@material-ui/core/ListItemText"
-import MenuIcon from "@material-ui/icons/Menu"
-import Button from "@material-ui/core/Button"
-import Typography from "@material-ui/core/Typography"
-import Tabs from "@material-ui/core/Tabs"
-import Tab from "@material-ui/core/Tab"
-import IconButton from "@material-ui/core/IconButton"
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import IconButton from "@material-ui/core/IconButton";
 
-import AccountCircleIcon from "@material-ui/icons/AccountCircle"
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart"
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
-import Hidden from "@material-ui/core/Hidden"
+import Hidden from "@material-ui/core/Hidden";
 
-import { makeStyles } from "@material-ui/core/styles"
-import useMediaQuery from "@material-ui/core/useMediaQuery"
+import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-import { Link } from "gatsby"
-import theme from "./theme"
+import { Link } from "gatsby";
+import theme from "./theme";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     textTransform: "capitalize",
   },
@@ -39,51 +39,50 @@ const useStyles = makeStyles(theme => ({
     marginLeft: "auto",
     marginRight: "auto",
   },
-}))
+}));
 
 function Header({ siteTitle }) {
-  const classes = useStyles()
-  const matchesSM = useMediaQuery(() => theme.breakpoints.down("sm"))
+  const classes = useStyles();
+  const matchesSM = useMediaQuery(() => theme.breakpoints.down("sm"));
   // const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
-  const [value, setValue] = useState(0)
-  const [drawerOpen, setDrawerOpen] = useState(false)
+  const [value, setValue] = useState(0);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const links = ["brands", "categories", "contact us", "About us"]
+  const links = ["brands", "categories", "contact us", "About us"];
 
   const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
+    setValue(newValue);
+  };
 
   const tabs = (
     <Tabs value={value} onChange={handleChange} className={classes.headerTabs}>
-      {links.map(link => {
-        return <Tab className={classes.tabsFont} label={link} />
+      {links.map((link) => {
+        return <Tab className={classes.tabsFont} label={link} />;
       })}
     </Tabs>
-  )
+  );
 
   const drawer = (
     <SwipeableDrawer
       open={drawerOpen}
       onOpen={() => {
-        setDrawerOpen(true)
+        setDrawerOpen(true);
       }}
       onClose={() => {
-        setDrawerOpen(false)
-      }}
-    >
+        setDrawerOpen(false);
+      }}>
       <List>
         {links.map((link, i) => {
           return (
             <ListItem button key={i}>
               <ListItemText primary={link} />
             </ListItem>
-          )
+          );
         })}
       </List>
     </SwipeableDrawer>
-  )
+  );
   return (
     <header>
       <AppBar elevation={0}>
@@ -107,18 +106,17 @@ function Header({ siteTitle }) {
             } */}
 
             <IconButton
-              onClick={() => (matchesSM ? setDrawerOpen(true) : null)}
-            >
+              onClick={() => (matchesSM ? setDrawerOpen(true) : null)}>
               {matchesSM ? <MenuIcon /> : <AccountCircleIcon />}
             </IconButton>
           </div>
         </Toolbar>
       </AppBar>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
 
 {
   /* <Tab className={classes.tabsFont} label="brands" />
